65a914b67bb45923641e9ca0bc77047c2101c0e2
David Froger <david.froger@mailoo.org>
2016-03-22 14:00:10 +0100

    Rename test_base to test_vector

--- geomalgo/base/test/test_vector.py
+++ geomalgo/base/test/test_vector.py
@@ -1,12 +1,12 @@
 import unittest
 
-import geomalgo as ga
+from geomalgo import Point
 
-class TestBase(unittest.TestCase):
+class TestVector(unittest.TestCase):
 
     def test_vector_from_point_sub_point(self):
-        A = ga.Point(1,2,3)
-        B = ga.Point(6,5,4)
+        A = Point(1,2,3)
+        B = Point(6,5,4)
         V = B - A
         self.assertEqual(V.x, 5)
         self.assertEqual(V.y, 3)
