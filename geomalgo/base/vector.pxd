c5165850fc50812c421351cdeffd8e7839934f11
David Froger <david.froger@mailoo.org>
2016-03-22 14:00:36 +0100

    Add Vector.from_points

--- geomalgo/base/vector.pxd
+++ geomalgo/base/vector.pxd
@@ -1,4 +1,8 @@
+from .point cimport Point
+
 cdef class Vector:
 
     cdef public:
         double x, y, z
+
+    cdef void from_points(self, Point A, Point B)
--- geomalgo/base/vector.pyx
+++ geomalgo/base/vector.pyx
@@ -5,4 +5,7 @@ cdef class Vector:
         self.y = y
         self.z = z
 
-
+    cdef void from_points(self, Point B, Point A):
+        self.x = B.x - A.x
+        self.y = B.y - A.y
+        self.z = B.z - A.z
