aa13aef02e95fd98437d51433de6cf3692610e2b
David Froger <david.froger@mailoo.org>
2016-06-20 22:31:51 +0200

    Add point projection on plane test

--- test/base3d/test_plane.py
+++ test/base3d/test_plane.py
@@ -5,6 +5,22 @@ from geomalgo import Point3D, Vector3D, Plane
 class TestPlane(unittest.TestCase):
 
     def test_creation(self):
+
+        A = Point3D(0, 0, 0)
+        B = Point3D(1, 0, 0)
+        C = Point3D(0, 1, 0)
+
+        P = Point3D(0.5, 0.5, 0)
+        M = Point3D(0.5, 0.5, 1)
+
+        normal = M - P
+
+        plane = Plane(point=P, normal=normal)
+
+        self.assertEqual(plane.point.x, 0.5)
+        self.assertEqual(plane.normal.z, 1)
+
+    def test_point_projection_on_horizontal_plane(self):
         """
                        M(0.5,0.5,1.)
                        +
@@ -27,25 +43,39 @@ class TestPlane(unittest.TestCase):
         P = Point3D(0.5, 0.5, 0)
         M = Point3D(0.5, 0.5, 1)
 
-        normal = M - P
+        plane = Plane(point=P, normal=M-P)
 
-        plane = Plane(point=P, normal=normal)
+        projection = plane.project_point(M)
+        self.assertAlmostEqual(projection.x, P.x)
+        self.assertAlmostEqual(projection.y, P.y)
+        self.assertAlmostEqual(projection.z, P.z)
 
-        self.assertEqual(plane.point.x, 0.5)
-        self.assertEqual(plane.normal.z, 1)
+    def test_point_projection_on_vertical_plane(self):
+        """
+                                     ^ z
 
-    def test_point_projection(self):
+                                     + C(0,0,1)
+                                   / |
+                                 /   |
+                               /     |
+        M(0.5,0.5,0.5)  x    +   P   |
+        x---------------<----|---+   + A(0,0,0)
+                             |     /
+                             |   /
+                             | /
+                             +  B(0,1,0)
 
-        A = Point3D(0, 0, 0)
-        B = Point3D(1, 0, 0)
-        C = Point3D(0, 1, 0)
+                         y
+        """
 
-        P = Point3D(0.5, 0.5, 0)
-        M = Point3D(0.5, 0.5, 1)
+        A = Point3D(0,0,0)
+        B = Point3D(0,1,0)
+        C = Point3D(0,0,1)
 
-        normal = M - P
+        P = Point3D(0. , 0.5, 0.5)
+        M = Point3D(0.5, 0.5, 0.5)
 
-        plane = Plane(point=P, normal=normal)
+        plane = Plane(point=P, normal=M-P)
 
         projection = plane.project_point(M)
         self.assertAlmostEqual(projection.x, P.x)
