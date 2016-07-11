a56ea3c02b14f884b1bb021cb562b0d947dd367f
David Froger <david.froger@performancevision.com>
2016-07-11 13:27:51 +0200

    Fix conda recipe on OSX

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,1 +1,7 @@
-pip install --prefix=$PREFIX .
+if [ "$(uname)" == "Darwin" ]
+then
+    LDFLAGS="-headerpad_max_install_names" \
+    pip install --prefix=$PREFIX .
+else
+    pip install --prefix=$PREFIX .
+fi
