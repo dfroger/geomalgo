186175baafa1d733073417960da90c25a50dc9d0
David Froger <david.froger@mailoo.org>
2016-10-13 14:01:09 +0200

    Prepare to more tests in test_winding.py

--- test/inclusion/test_winding.py
+++ test/inclusion/test_winding.py
@@ -1,6 +1,6 @@
 import unittest
 
-from geomalgo import Point2D, Polygon2D, Triangle2D
+from geomalgo import Point2D, Polygon2D, Triangle2D, Segment2D
 
 class TestSquareWinding(unittest.TestCase):
 
@@ -28,7 +28,8 @@ class TestSquareWinding(unittest.TestCase):
         P = Point2D(1.5, 0.5)
         self.assertFalse(self.polygon2d.includes_point(P))
 
-class TriangleWindingBase:
+
+class TriangleWinding(unittest.TestCase):
 
     """
     Base class for Polygon2D and Triange2D.
@@ -36,48 +37,36 @@ class TriangleWindingBase:
     Triangle2D use the same algorithm that Polygon2d, but specialized
     for optimization.
 
-    Derived class must implement the `build` method to create the `obj`
-    attribute, either a Triangle2D instance or a Polygon2D instance.
-
-      1 C
-        | \
-        |   \
-        |     \
-      0 A-------B
-        0       1
     """
 
-    def setUp(self):
-        A = Point2D(0,0)
-        B = Point2D(1,0)
-        C = Point2D(0,1)
+    def assert_inside(self, A, B, C, P):
+        triangle = Triangle2D(A, B, C)
+        polygon = Polygon2D.from_points2d([A,B,C])
 
-        self.build(A, B, C)
+        self.assertTrue(triangle.includes_point(P))
+        self.assertTrue(polygon.includes_point(P) )
 
-    def assert_inside(self, x, y):
-        P = Point2D(x,y)
-        self.assertTrue( self.obj.includes_point(P) )
+    def assert_outside(self, A, B, C, P):
+        triangle = Triangle2D(A, B, C)
+        polygon = Polygon2D.from_points2d([A,B,C])
 
-    def assert_outside(self, x, y):
-        P = Point2D(x,y)
-        self.assertFalse( self.obj.includes_point(P) )
+        self.assertFalse(triangle.includes_point(P))
+        self.assertFalse(polygon.includes_point(P) )
 
     def test_inside(self):
-        self.assert_inside(0.25, 0.25)
+        A = Point2D(0, 0)
+        B = Point2D(1, 0)
+        C = Point2D(0, 1)
+        P = Point2D(0.25, 0.25)
+        self.assert_inside(A, B, C, P)
 
     def test_outside(self):
-        self.assert_outside(1., 1.)
+        A = Point2D(0, 0)
+        B = Point2D(1, 0)
+        C = Point2D(0, 1)
+        P = Point2D(1., 1.)
+        self.assert_outside(A, B, C, P)
 
-@unittest.skip
-class TestTriangle2DWinding(TriangleWindingBase, unittest.TestCase):
-
-    def build(self, A, B, C):
-        self.obj = Triangle2D(A, B, C)
-
-class TestPolygon2DWinding(TriangleWindingBase, unittest.TestCase):
-
-    def build(self, A, B, C):
-        self.obj = Polygon2D.from_points2d([A,B,C])
 
 if __name__ == '__main__':
     unittest.main()
