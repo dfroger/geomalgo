6e6f93eff4f1dd57b3845002751b3ba83e06fb2c
David Froger <david.froger@mailoo.org>
2016-11-16 22:36:12 +0100

    Add Point2D to Line2D distance

--- geomalgo/base2d/line2d.pyx
+++ geomalgo/base2d/line2d.pyx
@@ -1,11 +1,16 @@
 from libc.stdlib cimport malloc, free
 
+from .point2d cimport subtract_points2d, point2d_plus_vector2d
+from .vector2d cimport CVector2D, dot_product2d, compute_norm2d
+
 cdef CLine2D* new_line2d():
     return <CLine2D*> malloc(sizeof(CLine2D))
+
 
 cdef void del_line2d(CLine2D* cline2d):
     if cline2d is not NULL:
         free(cline2d)
+
 
 cdef CLine2D* create_line2d(CPoint2D* A, CPoint2D* B):
     cdef:
@@ -13,6 +18,27 @@ cdef CLine2D* create_line2d(CPoint2D* A, CPoint2D* B):
     line.A = A
     line.B = B
     return line
+
+
+cdef double line2d_distance_point2d(CLine2D* AB, CPoint2D* P):
+    cdef:
+        CPoint2D Pb
+        CVector2D v, w
+        double c1, c2, b
+
+    subtract_points2d(&v, AB.B, AB.A)
+    subtract_points2d(&w, P, AB.A)
+
+    c1 = dot_product2d(&w, &v)
+    c2 = dot_product2d(&v, &v)
+    b = c1 / c2
+
+    point2d_plus_vector2d(&Pb, AB.A, b, &v)
+
+    subtract_points2d(&v, &Pb, P)
+
+    return compute_norm2d(&v)
+
 
 cdef class Line2D:
 
@@ -43,3 +69,6 @@ cdef class Line2D:
     def __str__(self):
         return "Line2D(({self.A.x},{self.A.y}),({self.B.x},{self.B.y}))" \
                .format(self=self)
+
+    def point_distance(Line2D self, Point2D P):
+        return line2d_distance_point2d(&self.cline2d, P.cpoint2d)
--- test/base2d/test_line2d.py
+++ test/base2d/test_line2d.py
@@ -30,5 +30,60 @@ class TestLine2D(unittest.TestCase):
         self.assertEqual(line.B.y, -2)
 
 
+class TestPoint2dDistance(unittest.TestCase):
+
+    def test_horizontal_line(self):
+        """
+              P
+
+        A-----------B
+        """
+
+        A = Point2D(1, 2)
+        B = Point2D(3, 2)
+        AB = Line2D(A, B)
+
+        P = Point2D(2, 4)
+        self.assertEqual(AB.point_distance(P), 2)
+
+        P = Point2D(10, 4)
+        self.assertEqual(AB.point_distance(P), 2)
+
+    def test_vertical_line(self):
+        """
+        B
+        |
+        |  B
+        |
+        A
+        """
+
+        A = Point2D(1, 2)
+        B = Point2D(1, 4)
+        AB = Line2D(A, B)
+
+        P = Point2D(3, 3)
+        self.assertEqual(AB.point_distance(P), 2)
+
+        P = Point2D(3, 10)
+        self.assertEqual(AB.point_distance(P), 2)
+
+
+    def test_on_horizontal_line(self):
+        """
+        A-----P-----B
+        """
+
+        A = Point2D(1, 2)
+        B = Point2D(3, 2)
+        AB = Line2D(A, B)
+
+        P = Point2D(2, 2)
+        self.assertEqual(AB.point_distance(P), 0)
+
+        P = Point2D(10, 2)
+        self.assertEqual(AB.point_distance(P), 0)
+
+
 if __name__ == '__main__':
     unittest.main()
