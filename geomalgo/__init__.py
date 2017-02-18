575dadaa2375a0188f4d91c3f4ab27ac28267791
David Froger <david.froger@mailoo.org>
2017-02-18 18:25:39 +0100

    Bump to version 0.3.0

--- geomalgo/__init__.py
+++ geomalgo/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.1dev'
+__version__ = '0.3.0'
 
 from .base1d import *
 from .base2d import *
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, Extension
 
 from Cython.Build import cythonize
 
-__version__ = '0.2.1dev'
+__version__ = '0.3.0'
 
 def module_name(fp):
     """
